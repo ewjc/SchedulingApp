8c0fbbcfab643710c68906c6100826b84506a0bc
Eric Spencer Wong <ericswong7@gmail.com>
2020-07-10 12:05:22 -0700

    Initial Commit

---
+++ Scheduling App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scheduling App
+//
+//  Created by Eric Wong on 7/10/20.
+//  Copyright © 2020 Eric Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
